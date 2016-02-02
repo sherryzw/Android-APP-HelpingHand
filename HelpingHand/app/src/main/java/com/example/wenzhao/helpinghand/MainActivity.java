ad30d947bf64476492a151ab7a97f5bcf4dc552e
sherryzw <zhaowen.tju@gmail.com>
2016-02-02 14:01:44 -0500

    Test

--- HelpingHand/app/src/main/java/com/example/wenzhao/helpinghand/MainActivity.java
+++ HelpingHand/app/src/main/java/com/example/wenzhao/helpinghand/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    /*can you hear me ????*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
