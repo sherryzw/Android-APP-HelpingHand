c3f6ad0da25c926d8b6be70324f3157f156237b3
jimwong234 <jimwong234@gmail.com>
2016-03-03 12:27:24 -0500

    Update for rating bar

--- HelpingHand/app/src/main/java/com/example/wenzhao/helpinghand/ble/pro/HelpingHand/ResultActivity.java
+++ HelpingHand/app/src/main/java/com/example/wenzhao/helpinghand/ble/pro/HelpingHand/ResultActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.example.ti.ble.sensortag.R;
@@ -14,6 +15,7 @@ public class ResultActivity extends Activity {
     private Button btnReplay;
     private double finalRatio = 0;
     TextView resultText;
+    RatingBar ratingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +23,13 @@ public class ResultActivity extends Activity {
         setContentView(R.layout.activity_result);
         resultText = (TextView)findViewById(R.id.textView17);
         btnReplay = (Button)findViewById(R.id.btn_replay);
+        ratingBar = (RatingBar)findViewById(R.id.ratingBar);
         for(int i = 1;i < DeviceActivity.ratioOverTime.size();i++) finalRatio += DeviceActivity.ratioOverTime.get(i);
         finalRatio /= (DeviceActivity.ratioOverTime.size() - 1);
+        setRating(finalRatio);
 
         resultText.setText("Finished in " + String.valueOf(DeviceActivity.time) + " s. "
-                +"Your "+ InputFragment.curChild.getWeakArm() + " hand did "
+                + "Your " + InputFragment.curChild.getWeakArm() + " hand did "
                 + String.valueOf(finalRatio) + "% of the work! Try again to beat your" +
                 " score.");
 
@@ -35,5 +39,18 @@ public class ResultActivity extends Activity {
                 finish();
             }
         });
+    }
+
+    private void setRating(double rating){
+        if((rating >=0 && rating <5)||(rating >95 && rating <=100)) ratingBar.setRating((float)0.5);
+        if((rating >=5 && rating <10)||(rating >90 && rating <=95)) ratingBar.setRating((float)1);
+        if((rating >=10 && rating <15)||(rating >85 && rating <=90)) ratingBar.setRating((float)1.5);
+        if((rating >=15 && rating <20)||(rating >80 && rating <=85)) ratingBar.setRating((float)2);
+        if((rating >=20 && rating <25)||(rating >75 && rating <=80)) ratingBar.setRating((float)2.5);
+        if((rating >=25 && rating <30)||(rating >70 && rating <=75)) ratingBar.setRating((float)3);
+        if((rating >=30 && rating <35)||(rating >65 && rating <=70)) ratingBar.setRating((float)3.5);
+        if((rating >=35 && rating <40)||(rating >60 && rating <=65)) ratingBar.setRating((float)4);
+        if((rating >=40 && rating <45)||(rating >55 && rating <=60)) ratingBar.setRating((float)4.5);
+        if((rating >=45 && rating <=50)||(rating >50 && rating <=55)) ratingBar.setRating((float)5);
     }
 }
