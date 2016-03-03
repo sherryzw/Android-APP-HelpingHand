721a07430684b82a893dea778b062671799bb72c
jimwong234 <jimwong234@gmail.com>
2016-03-03 12:54:20 -0500

    Update pic

--- HelpingHand/app/src/main/java/com/example/wenzhao/helpinghand/ble/pro/HelpingHand/ResultActivity.java
+++ HelpingHand/app/src/main/java/com/example/wenzhao/helpinghand/ble/pro/HelpingHand/ResultActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
@@ -16,6 +17,7 @@ public class ResultActivity extends Activity {
     private double finalRatio = 0;
     TextView resultText;
     RatingBar ratingBar;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +26,10 @@ public class ResultActivity extends Activity {
         resultText = (TextView)findViewById(R.id.textView17);
         btnReplay = (Button)findViewById(R.id.btn_replay);
         ratingBar = (RatingBar)findViewById(R.id.ratingBar);
+        imageView = (ImageView)findViewById(R.id.imageView2);
         for(int i = 1;i < DeviceActivity.ratioOverTime.size();i++) finalRatio += DeviceActivity.ratioOverTime.get(i);
         finalRatio /= (DeviceActivity.ratioOverTime.size() - 1);
+        setImage(finalRatio);
         setRating(finalRatio);
 
         resultText.setText("Finished in " + String.valueOf(DeviceActivity.time) + " s. "
@@ -52,5 +56,10 @@ public class ResultActivity extends Activity {
         if((rating >=35 && rating <40)||(rating >60 && rating <=65)) ratingBar.setRating((float)4);
         if((rating >=40 && rating <45)||(rating >55 && rating <=60)) ratingBar.setRating((float)4.5);
         if((rating >=45 && rating <=50)||(rating >50 && rating <=55)) ratingBar.setRating((float)5);
+    }
+
+    private void setImage(double rating){
+        if((rating >=0 && rating <25)||(rating >75 && rating <=100)) imageView.setImageResource(R.drawable.sadface);
+        if((rating >=25 && rating <50)||(rating >=50 && rating <=75)) imageView.setImageResource(R.drawable.smileface);
     }
 }
